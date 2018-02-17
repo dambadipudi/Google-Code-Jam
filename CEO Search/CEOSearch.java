c1cf72baff57efe9852b7c6be236f9b231096c07
dambadipudi <divya.shastry@gmail.com>
2018-02-17 13:20:52 -0800

    Adding files for CEO Search problem
    It still doesn't work for large data set. Hoping to find a fix for this soon.

---
+++ CEO Search/CEOSearch.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class CEOSearch {
+	
+	public static long getMinimumCEOExpLevel(HashMap<Long, Long> experienceEmployeeMap) {
+		
+		long countOfEmployeesLeft = 0;
+		long maxExpLevel = 0;
+
+		for(Long currentExperience : experienceEmployeeMap.keySet()) {
+			long currentEmployees = experienceEmployeeMap.get(currentExperience);
+			countOfEmployeesLeft = currentEmployees + Math.max(0, countOfEmployeesLeft - currentExperience*currentEmployees);
+			if(currentExperience > maxExpLevel) {
+				maxExpLevel = currentExperience;
+			}
+		}
+				
+		return Math.max(countOfEmployeesLeft, maxExpLevel+1);
+	}
+ 
+	public static void main(String [] args) throws IOException {
+		Scanner in = new Scanner(new File(args[0]));
+		PrintStream fileOut = new PrintStream(new File(args[1]));
+
+		//Read input from scanner
+		int testCaseSize = in.nextInt();
+		long [] resultArray = new long[testCaseSize + 1];
+
+		for(int curTestIndex = 1; curTestIndex <= testCaseSize; curTestIndex++) {
+
+			//Get input for each test case
+			int totalExperiences = in.nextInt();
+			HashMap<Long, Long> experienceEmployeeMap = new HashMap<Long, Long>(); 
+
+			for(long arrayIndex = 0; arrayIndex < totalExperiences; arrayIndex++) {
+				long employeeCount = in.nextLong();
+				long experienceLevel = in.nextInt();
+				experienceEmployeeMap.put(experienceLevel, employeeCount);
+			}		
+	
+			// Call the function for each test case
+			 resultArray[curTestIndex] = getMinimumCEOExpLevel(experienceEmployeeMap);
+		}
+		in.close();
+
+		System.setOut(fileOut);
+		//Print to output stream
+		for(int curTestIndex = 1; curTestIndex <= testCaseSize; curTestIndex++) {	
+			System.out.println("Case #" + curTestIndex+ ": "+resultArray[curTestIndex]);
+		}
+		fileOut.close();
+	}
+}		
